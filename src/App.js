07d04035c68d166cf139893935eb1dde9b9d5e6d
Echochris234 <christianvargas1595@gmail.com>
2021-01-07 01:57:56 -0500

    more

--- src/App.js
+++ src/App.js
@@ -17,20 +17,20 @@ import './App.css';
 function Navigation(props) {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-      <Link className="navbar-brand" to="/personal-page">Christian's Site</Link>
+      <Link className="navbar-brand" to="/home">Christian's Site</Link>
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <NavLink className="nav-link" exact to="/personal-page">
+          <NavLink className="nav-link" exact to="/home">
             Home
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" exact to="/personal-page/about-us">
+          <NavLink className="nav-link" exact to="/about-us">
             About
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" exact to="/personal-page/assignments">
+          <NavLink className="nav-link" exact to="/assignments">
             Assignments
           </NavLink>
         </li>
@@ -50,9 +50,9 @@ class App extends React.Component {
           <div className="row justify-content-center">
             
             <Switch>
-              <Route path="/personal-page/about-us" component={AboutUsPage} />
-              <Route path="/personal-page/assignments" component={Assignments} />
-              <Route path="/personal-page" component={HomePage} />
+              <Route path="/about-us" component={AboutUsPage} />
+              <Route path="/assignments" component={Assignments} />
+              <Route path="/home" component={HomePage} />
             </Switch>
           </div>
         </div>
